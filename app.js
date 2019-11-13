c83ba2012f940f23149d6899c4148935ade4b396
727021 <1834544+727021@users.noreply.github.com>
2019-11-13 10:49:00 -0700

    Redirect to form

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', (req, res) => res.redirect('/form.html'));
 app.use('/users', usersRouter);
 app.use('/getRate', rateRouter)
 
